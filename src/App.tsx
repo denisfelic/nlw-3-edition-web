7399016951eb7e316bb32931d46380b71e659973
Denis Feliciano <denisf.salles@gmail.com>
2020-10-13 02:05:00 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import { FiArrowRight} from 'react-icons/fi';
 
-// CSS
-import './styles/global.css';
-import './styles/pages/landing.css';
-
-// IMAGES
-import logoImage from './images/logo.svg'
+import Routes from './routes';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImage} alt="Happy Logo" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a className="enter-app" href="" > <FiArrowRight size={26} color="rgba(0,0,0,0.6)" /> </a>
-      </div>
-    </div>
+   <Routes />
   );
 }
 
